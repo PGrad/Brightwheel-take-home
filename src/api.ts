2a8df168437f02db7b27c3323d54a362c39079a2
Paul Grad <paulgrad3rd@gmail.com>
2024-04-18 14:45:33 -0700

    create api utilities
    I create functions to
    abstract out calling the API.

---
+++ src/api.ts
@@ -0,0 +1,45 @@
+const API_ENDPOINT = 'http://localhost:3001';
+
+export async function getListingsByQuery(query: string, pageNo: number = 1) {
+    const response = await fetch(`${API_ENDPOINT}/search?name_like=^${query}&_limit=10&_page=${pageNo}`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch listings.');
+    }
+
+    return await response.json();
+}
+
+export async function getAllListings(pageNo: number = 1) {
+    const response = await fetch(`${API_ENDPOINT}/search?_limit=10&_page=${pageNo}`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch listings.');
+    }
+
+    return await response.json();
+}
+
+export async function changeStarStatus(id: string, status: boolean): Promise<void> {
+    const response = await fetch(`${API_ENDPOINT}/search/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ isStarred: status }),
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to update star status.');
+    }
+
+    return;
+}
+
+export function debounce(fn: any, delay: number) {
+    let timeoutId: NodeJS.Timeout;
+    return (...args: any[]) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => fn(...args), delay);
+    };
+  };
